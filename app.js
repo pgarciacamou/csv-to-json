1cec1701193b7c2957c6a162d7e0772946865e20
Pablo Garcia <pgarciacamou@gmail.com>
2015-03-30 13:23:41 -0700

    Working with multiple-files

--- app.js
+++ app.js
@@ -2,48 +2,35 @@
 var Converter = require("csvtojson").core.Converter;
 var fs = require("fs");
 
-//new converter instance 
-var csvConverter = new Converter({constructResult:true});
-
 if(!process.argv[2]) throw "Must pass directory";
 var csvFileDirectory = process.argv[2].toString();
 
-var currentFileName;
-
-// process.argv.forEach(function (val, index, array) {
-// });
-
-
-//end_parsed will be emitted once parsing finished 
-csvConverter.on("end_parsed",function(jsonObj){
-  fs.writeFile(currentFileName + ".json", JSON.stringify(jsonObj), function(err) {
-    if(err) return console.log(err);
-    console.log("The file was saved!");
-  }); 
-});
-
 var getAllFiles = function(dir) {
-    var results = [];
+  var results = [];
 
-    fs.readdirSync(dir).forEach(function(file) {
+  fs.readdirSync(dir).forEach(function(file) {
 
-        file = dir+'/'+file;
-        var stat = fs.statSync(file);
+    file = dir+'/'+file;
+    var stat = fs.statSync(file);
 
-        if (stat && stat.isDirectory()) {
-            results = results.concat(getAllFiles(file))
-        } else results.push(file);
+    if (stat && stat.isDirectory()) {
+      results = results.concat(getAllFiles(file))
+    } else results.push(file);
 
-    });
+  });
 
-    return results;
+  return results;
 };
 var files = getAllFiles(csvFileDirectory);
 
 files.forEach(function(fileName){
   if(!/.csv$/.test(fileName)) return;
-	currentFileName = fileName;
-	var fileStream = fs.createReadStream(fileName);
-	//read from file
-	fileStream.pipe(csvConverter);
+  var csvConverter = new Converter({constructResult:true});
+  csvConverter.on("end_parsed",function(jsonObj){
+    fs.writeFile(fileName + ".json", JSON.stringify(jsonObj), function(err) {
+      if(err) return console.log(err);
+      console.log("The file was saved!");
+    }); 
+  });
+  fs.createReadStream(fileName).pipe(csvConverter);
 });
